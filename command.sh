310f11fb1dd87423d6463ae27c46d4b4ea4a73a5
leon <zhougong410@126.com>
2016-06-28 18:29:54 +0800

    1. add git command.sh 2. add data_base.design 3. add refrence.link

---
+++ command.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Created By:      Leon Zhou
+# Created Time:    2016-06-27 10:16:44
+# Modified Time:   2016-06-27 10:18:35
+git init
+git add file
+git commit
+#[-m]
+git push -u origin master
